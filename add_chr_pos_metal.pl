d1003a4d4934a44dbd04a9fb0d2b180460b7ba66
vaqm2 <appadurai.vivek@gmail.com>
2021-11-12 15:56:04 +0100

    Code dump

---
+++ add_chr_pos_metal.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use IO::File;
+
+my $bim  = IO::File->new("$ARGV[0]"); # BIM FILE
+my $dict = {};
+
+while(my $line = $bim->getline)
+{
+    chomp($line);
+    my @lineContents     = split(/\s+/, $line);
+    my $chromosome       = $lineContents[0];
+    my $snp              = $lineContents[1];
+    my $position         = $lineContents[3];
+    $dict->{$snp}->{chr} = $chromosome;
+    $dict->{$snp}->{pos} = $position;
+}
+
+$bim->close;
+
+my $nh = IO::File->new("$ARGV[1]"); # METAL OUTPUT
+
+while(my $line = $nh->getline)
+{
+    chomp($line);
+    if($line =~ /^Marker/)
+    {
+        print "CHR"."\t";
+        print "BP"."\t";
+        print "SNP"."\t";
+        print "A1"."\t";
+        print "A2"."\t";
+        print "N"."\t";
+        print "Z"."\t";
+        print "P"."\t";
+        print "DIRECTION"."\n";
+    }
+    else
+    {
+        my @lineContents = split(/\s+/, $line);
+        my $snp          = $lineContents[0];
+
+        print $dict->{$snp}->{chr}."\t";
+        print $dict->{$snp}->{pos}."\t";
+    }
+
+    print $line."\n";
+}
+
+$nh->close;
---
+++ plot_metal_assoc.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+require(dplyr, quietly = TRUE)
+require(qqman, quietly = TRUE)
+require(ggplot2, quietly = TRUE)
+require(data.table, quietly = TRUE)
+
+args = commandArgs(trailingOnly = TRUE)
+assoc = fread(args[1], header = TRUE) 
+out_prefix = args[2]
+
+if("Is.SPA.converge" %in% colnames(assoc)) {
+    assoc = assoc %>% filter(Is.SPA.converge == 1)
+}
+
+if("Is.converge" %in% colnames(assoc)) {
+    assoc = assoc %>% filter(Is.converge == 1)
+}
+assoc = assoc %>% select(CHR, SNP, BP, P)
+
+png(paste(out_prefix, "Manhattan.png", sep = "_"),
+    width = 8, 
+    height = 5, 
+    units = "in", 
+    res = 300)
+manhattan(assoc)
+dev.off()
+
+png(paste(out_prefix, "QQ.png", sep = "_"), 
+    width = 5, 
+    height = 5, 
+    units = "in", 
+    res = 300)
+qq(assoc$P)
+dev.off()
