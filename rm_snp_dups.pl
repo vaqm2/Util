4e5a58967d48e0c650ed35fa795aec8f5eb8d447
vaqm2 <appadurai.vivek@gmail.com>
2021-11-23 16:20:53 +0100

    new code

---
+++ rm_snp_dups.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $dict = {};
+
+while(<>)
+{
+    my $line = chomp($_);
+    my @lineContents = split(/\s+/, $line);
+    my $snp_id = $lineContents[1];
+
+    if(exists $dict->{$snp_id]})
+    {
+        next;
+    }
+    else
+    {
+        print $line."\n";
+    }
+}
