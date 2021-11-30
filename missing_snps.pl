2152d5d4b583193d7f836a38b0bee14731585ac0
vaqm2 <appadurai.vivek@gmail.com>
2021-11-30 10:13:24 +0100

    new code

---
+++ missing_snps.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use IO::File;
+
+my $fh = IO::File->new("$ARGV[0]");
+my $dict = {};
+
+while(my $line = $fh->getline)
+{
+    chomp($line);
+    my @lineContents = split(/\s+/, $line);
+    my $snp = $lineContents[1];
+    $dict->{$snp} = 1;
+}
+
+$fh->close;
+
+open(IN, "zcat $ARGV[1] |");
+
+while(<IN>)
+{
+    my $line = chomp($_);
+    if($line =~ /^CHR/)
+    {
+        next;
+    }
+    else
+    {
+        my @lineContents = split(/\s+/, $line);
+        my $snp = $lineContents[1];
+
+        if(exists $dict->{$snp})
+        {
+            next;
+        }
+        else
+        {
+            print $snp." Missing"."\n";
+        }
+    }
+}
+
+close(IN);
