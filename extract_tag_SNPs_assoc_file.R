4f6c2a6939d0172adf800d8bad16fce7901088c9
vaqm2 <appadurai.vivek@gmail.com>
2021-12-17 13:22:24 +0100

    code updates

---
+++ extract_tag_SNPs_assoc_file.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+require(data.table)
+require(dplyr)
+
+args  = commandArgs(trailingOnly = TRUE)
+
+assoc = fread(args[1], header = TRUE)
+snps  = fread(args[2], header = FALSE)
+out   = args[3]
+
+colnames(snps) = c("SNP")
+tag_assoc = inner_join(snps, assoc, by = c("SNP"))
+
+write.table(tag_assoc, out, row.names = F, quote = F, sep= " ")
