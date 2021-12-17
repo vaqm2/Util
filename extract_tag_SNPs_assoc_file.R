00365bbcc206a4f8e4281e1614b8f2cf059c775c
vaqm2 <appadurai.vivek@gmail.com>
2021-12-17 14:13:52 +0100

    sort cols

--- extract_tag_SNPs_assoc_file.R
+++ extract_tag_SNPs_assoc_file.R
@@ -10,6 +10,7 @@ snps  = fread(args[2], header = FALSE)
 out   = args[3]
 
 colnames(snps) = c("SNP")
-tag_assoc = inner_join(snps, assoc, by = c("SNP"))
+tag_assoc = inner_join(snps, assoc, by = c("SNP")) %>%
+    arrange(CHR, BP)
 
 write.table(tag_assoc, out, row.names = F, quote = F, sep= " ")
