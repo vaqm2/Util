3e972b91947975aa1d43412faaf1b800e44a3db0
vaqm2 <appadurai.vivek@gmail.com>
2022-02-10 13:05:59 +0100

    new code

---
+++ split_sumstats_even_odd.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+
+def main():
+    sumstats = sys.argv[1]
+    fh = open(sumstats, 'r')
+    even_out = open(sys.argv[2] + "even.txt", 'w')
+    odd_out = open(sys.argv[2] + "odd.txt", 'w')
+
+    for line in fh:
+        if(line.startswith("CHR")):
+            even_out.write(line)
+            odd_out.write(line)
+        else:
+            lineContents = line.split()
+            chromosome = lineContents[0]
+            if chromosome.isnumeric():
+                if(chromosome % 2 == 0):
+                    even_out.write(line)
+                else:
+                    odd_out.write(line)
+            else:
+                sys.stdout("Skipping non-autosomes: " + line)
+
+    fh.close()
+    even_out.close()
+    odd_out.close()
+
+
+if __name__ == '__main__':
+    main()
