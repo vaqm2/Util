1f966f577ddfa9bc1b3c0816f7a78f0c4ce98706
vaqm2 <appadurai.vivek@gmail.com>
2022-02-14 12:07:10 +0100

    remove a col

--- keep_sem_cols.R
+++ keep_sem_cols.R
@@ -5,6 +5,6 @@ require(data.table)
 
 args = commandArgs(trailingOnly = TRUE)
 file = fread(args[1], header = T)
-file = file %>% select(CHR, POS, RSID, A1, A2, Z, P, SE, INFO)
+file = file %>% select(CHR, POS, RSID, A1, A2, Z, P, SE)
 file = file %>% rename(SNP = RSID)
 write.table(file, args[2], sep = " ", row.names = F, quote = F)
