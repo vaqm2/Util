d7aceb223a0a045eba0e1c81959c3054b93af77f
vaqm2 <appadurai.vivek@gmail.com>
2022-02-15 15:20:40 +0100

    new code

--- pve_cov.R
+++ pve_cov.R
@@ -2,9 +2,10 @@
 
 require(dplyr)
 
-args  = commandArgs(trailingOnly = TRUE)
-pheno = args[1]
-covar = args[2]
+args    = commandArgs(trailingOnly = TRUE)
+pheno   = read.table(args[1], header = T)
+covar   = read.table(args[2], header = T)
 phe_cov = inner_join(pheno, covar, by = c("fid", "iid"))
+
 summary(lm(phe_cov$pheno ~ phe_cov$age))$adj.r.squared
 summary(lm(phe_cov$pheno ~ phe_cov$sex))$adj.r.squared
