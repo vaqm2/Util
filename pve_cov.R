444360525de18b01f06b8f4d42911287ccef7acd
vaqm2 <appadurai.vivek@gmail.com>
2022-02-16 09:52:53 +0100

    code mods

--- pve_cov.R
+++ pve_cov.R
@@ -2,10 +2,22 @@
 
 require(dplyr)
 
-args    = commandArgs(trailingOnly = TRUE)
-pheno   = read.table(args[1], header = T)
-covar   = read.table(args[2], header = T)
-phe_cov = inner_join(pheno, covar, by = c("fid", "iid"))
+args      = commandArgs(trailingOnly = TRUE)
+pheno     = read.table(args[1], header = T)
+covar     = read.table(args[2], header = T)
+phe_cov   = inner_join(pheno, covar, by = c("fid", "iid"))
+full_r2   = summary(lm(data = phe_cov, pheno ~ . -fid -iid))$adj.r.squared
+wo_age_r2 = summary(lm(data - phe_cov, pheno ~ . -fid -iid -age))$adj.r.squared
+wo_sex_r2 = summary(lm(data = phe_cov, pheno ~ . -fid -iid -sex))$adj.r.squared
+age_r2    = full_r2 - wo_age_r2
+sex_r2    = full_r2 - wo_sex_r2
 
-summary(lm(phe_cov$pheno ~ phe_cov$age))$adj.r.squared
-summary(lm(phe_cov$pheno ~ phe_cov$sex))$adj.r.squared
+cat("Proportion of Variance Explained by all Covariates : ")
+cat(full_r2)
+cat("\n")
+cat("Proportion of Variance Explained by age : ")
+cat(age_r2)
+cat("\n")
+cat("Proportion of Variance Explained by sex : ")
+cat(sex_r2)
+cat("\n")
