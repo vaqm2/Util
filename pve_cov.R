f6692210062479e60f26a07070bf645c7ac64336
vaqm2 <appadurai.vivek@gmail.com>
2022-02-16 10:52:09 +0100

    code mods

--- pve_cov.R
+++ pve_cov.R
@@ -7,7 +7,7 @@ pheno     = read.table(args[1], header = T)
 covar     = read.table(args[2], header = T)
 phe_cov   = inner_join(pheno, covar, by = c("fid", "iid"))
 full_r2   = summary(lm(data = phe_cov, pheno ~ . -fid -iid))$adj.r.squared
-wo_age_r2 = summary(lm(data - phe_cov, pheno ~ . -fid -iid -age))$adj.r.squared
+wo_age_r2 = summary(lm(data = phe_cov, pheno ~ . -fid -iid -age))$adj.r.squared
 wo_sex_r2 = summary(lm(data = phe_cov, pheno ~ . -fid -iid -sex))$adj.r.squared
 age_r2    = full_r2 - wo_age_r2
 sex_r2    = full_r2 - wo_sex_r2
