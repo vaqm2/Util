7785618c506cf0951c1e90bf4c528841f5c38bcc
vaqm2 <appadurai.vivek@gmail.com>
2022-02-25 11:51:02 +0100

    new code :-)

---
+++ pairwise_cc_pheno.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use IO::File;
+use Getopt::Long;
+
+sub usage() {
+    print "Missing required files!"."\n"; 
+    print "\n"."USAGE: perl $0 --pheno1 <Phenotype1.txt> --pheno2 <Phenotype2.txt> --out <Output.txt>"."\n\n";
+    exit;
+}
+
+sub read_phenotypes {
+    my $file = shift;
+    my $dict = {};
+
+    my $fh = IO::File->new($file) || die "ERROR: Phenotype file $file not found!!"."\n";
+
+    while(my $line = $fh->getline) {
+        chomp($line);
+
+        if($line =~ /^IID/i) {
+            next;
+        }
+        else {
+            my @lineContents = split(/\s+/, $line);
+            my $iid          = $lineContents[0];
+            my $phenotype    = $lineContents[$#lineContents];
+
+            if($phenotype == 0) {
+                next;
+            }
+            else {
+                $dict->{$iid}    = $line;
+            }
+        }
+    }
+
+    $fh->close;
+    return $dict;
+}
+
+my ($pheno1, $pheno2, $out);
+
+if(!$pheno1 || !$pheno2 || !$out) {
+    usage();
+}
+
+
+GetOptions(
+    "pheno1=s" => \$pheno1,
+    "pheno2=s" => \$pheno2,
+    "out=s" => \$out
+);
+
+my $pheno1_dict = read_phenotypes($pheno1);
+my $pheno2_dict = read_phenotypes($pheno2);
+my $out_fh = IO::File->new("> $out") || die "ERROR: Cannot create output file: $out"."!!\n";
+
+for my $index(keys %$pheno1_dict) {
+    if(exists $pheno2_dict->{$index}) {
+        next;
+    }
+    else {
+        $pheno1_dict->{$index} =~ s/\s+/" "/;
+        print $out_fh $pheno1_dict->{$index}."\n";
+    }
+}
+
+for my $index(keys %$pheno2_dict) {
+    if(exists $pheno1_dict->{$index}) {
+        next;
+    }
+    else {
+        my @lineContents = split(/\s+/, $pheno2_dict->{$index});
+        my $phenotype = $lineContents[$#lineContents] - 1;
+
+        for my $line_index(0..($#lineContents - 1)) {
+            print $out_fh $lineContents[$line_index]." ";
+        }
+
+        print $out_fh $phenotype."\n";
+    }
+}
+
+$out_fh->close;
