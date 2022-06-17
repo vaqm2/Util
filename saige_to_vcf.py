5e1019f42675051ca0cc0d05c92405307ba2ff0b
vaqm2 <appadurai.vivek@gmail.com>
2022-06-17 11:26:57 +0200

    debug

--- saige_to_vcf.py
+++ saige_to_vcf.py
@@ -24,17 +24,17 @@ def main():
 
     for line in fr:
         if line.startswith("CHR"):
-            print("##fileformat=VCFv4.3\n")
-            print("##fileDate=" + date.today().strftime("%y%m%d") + "\n")
-            print("##source=saige_to_vcf.py\n")
-            print("##FORMAT=<ID=ES>,Number=A,Type=FLOAT,Description=\"Effect Size of ALT\"\n")
-            print("##FORMAT=<ID=ES>,Number=A,Type=FLOAT,Description=\"Standard Error of Effect Size\"\n")
-            print("##FORMAT=<ID=LP>,Number=A,Type=FLOAT,Description=\"Negative log of P-value for Effect Size\"\n")
-            print("##FORMAT=<ID=AFKG>,Number=A,Type=FLOAT,Description=\"Frequency of ALT\"\n")
-            print("##FORMAT=<ID=DR2>,Number=A,Type=FLOAT,Description=\"Imputation INFO score\"\n")
-            print("##FORMAT=<ID=SS>,Number=A,Type=FLOAT,Description=\"Sample Size\"\n")
-            print("#CHR\tBP\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t")
-            print(args.out + "\n")
+            fw.write("##fileformat=VCFv4.3\n")
+            fw.write("##fileDate=" + date.today().strftime("%y%m%d") + "\n")
+            fw.write("##source=saige_to_vcf.py\n")
+            fw.write("##FORMAT=<ID=ES>,Number=A,Type=FLOAT,Description=\"Effect Size of ALT\"\n")
+            fw.write("##FORMAT=<ID=ES>,Number=A,Type=FLOAT,Description=\"Standard Error of Effect Size\"\n")
+            fw.write("##FORMAT=<ID=LP>,Number=A,Type=FLOAT,Description=\"Negative log of P-value for Effect Size\"\n")
+            fw.write("##FORMAT=<ID=AFKG>,Number=A,Type=FLOAT,Description=\"Frequency of ALT\"\n")
+            fw.write("##FORMAT=<ID=DR2>,Number=A,Type=FLOAT,Description=\"Imputation INFO score\"\n")
+            fw.write("##FORMAT=<ID=SS>,Number=A,Type=FLOAT,Description=\"Sample Size\"\n")
+            fw.write("#CHR\tBP\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t")
+            fw.write(args.out + "\n")
         else:
             assoc_contents       = line.split()
             chromosome           = assoc_contents[0]
@@ -42,29 +42,29 @@ def main():
             snp                  = assoc_contents[2]
             ref_allele           = assoc_contents[3]
             alt_allele           = assoc_contents[4]
-            qual                 = "100"
+            qual                 = "."
             filter               = "PASS"
             alt_allele_frequency = assoc_contents[6]
             imputation_info      = assoc_contents[7]
             sample_size          = assoc_contents[8]
             effect_size          = assoc_contents[9]
             standard_error       = assoc_contents[10]
-            negative_logp        = -1 * log10(assoc_contents[12])
+            negative_logp        = -1 * log10(float(assoc_contents[12]))
 
-            print(chromosome + "\t")
-            print(position + "\t")
-            print(snp + "\t")
-            print(ref_allele + "\t")
-            print(alt_allele + "\t")
-            print(qual + "\t")
-            print(filter + "\t")
-            print("." + "\t")
-            print("ES:SE:EP:AFKG"+ "\t")
-            print(effect_size + ":")
-            print(standard_error + ":")
-            print(negative_logp + ":")
-            print(alt_allele_frequency + ":")
-            print(imputation_info + "\n")
+            fw.write(chromosome + "\t")
+            fw.write(position + "\t")
+            fw.write(snp + "\t")
+            fw.write(ref_allele + "\t")
+            fw.write(alt_allele + "\t")
+            fw.write(qual + "\t")
+            fw.write(filter + "\t")
+            fw.write("." + "\t")
+            fw.write("ES:SE:EP:AFKG"+ "\t")
+            fw.write(effect_size + ":")
+            fw.write(standard_error + ":")
+            fw.write(negative_logp + ":")
+            fw.write(alt_allele_frequency + ":")
+            fw.write(imputation_info + "\n")
 
     fr.close()
     fw.close()
