f472e3ceabe1720d94a620c80a1c9605ac1cea33
vaqm2 <appadurai.vivek@gmail.com>
2022-06-20 11:33:52 +0200

    new code

--- pheno_counts.R
+++ pheno_counts.R
@@ -13,8 +13,8 @@ colnames(fam_2015i) = c("FID", "IID", "M", "F", "GENDER", "PHENO")
 
 n_2012 = inner_join(fam_2012, pheno, by = c("IID")) %>% unique()
 n_2015i = inner_join(fam_2015i, pheno, by = c("IID")) %>% unique()
-cases_2012 = table(n_2012[ncol(n_2012)])[1]
-cases_2015i = table(n_2015i[ncol(n_2015i)])[1]
+cases_2012 = table(n_2012[ncol(n_2012)])[2]
+cases_2015i = table(n_2015i[ncol(n_2015i)])[2]
 
 print(paste0("iPSYCH2012 N: ", nrow(n_2012), " Cases : ", cases_2012))
 print(paste0("iPSYCH2015i N: ", nrow(n_2015i), " Cases : ",cases_2015i))
