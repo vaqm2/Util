c08320367ded073f4066e48a51eba6d10ba96c0a
vaqm2 <appadurai.vivek@gmail.com>
2022-06-20 11:00:58 +0200

    new code

---
+++ pheno_counts.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+require(dplyr)
+
+args          = commandArgs(trailingOnly = TRUE)
+pheno         = read.table(args[1], header = T)
+fam_2012      = read.table(args[2], header = T)
+fam_2015i     = read.table(args[3], header = T)
+n_2012        = semi_join(fam_2012, pheno, by = c("IID")) %>% nrow()
+n_2015i       = semi_join(fam_2015i, pheno, by = c("IID")) %>% nrow()
+n_cases_2012  = semi_join(fam_2012, pheno, by = c("IID")) %>% filter(PHENOTYPE == 1) %>% nrow()
+n_cases_2015i = semi_join(fam_2015i, pheno, by = c("IID")) %>% filter(PHENOTYPE == 1) %>% nrow()
+
+print(paste0("iPSYCH2012 N: ", n_2012, "Cases : ", n_cases_2012))
+print(paste0("iPSYCH2015i N: ", n_2015i, "Cases : ", n_cases_2015i))
