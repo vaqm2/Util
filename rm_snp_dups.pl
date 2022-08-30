444f46a2510742ec8970790074f23612b1f905ef
vaqm2 <appadurai.vivek@gmail.com>
2022-08-30 15:55:37 +0200

    changedh eader

--- rm_snp_dups.pl
+++ rm_snp_dups.pl
@@ -21,17 +21,11 @@ while(<IN>)
     }
 
     my @lineContents = split(/\s+/, $line);
-    my $snp_id       = $lineContents[2];
     my $chr          = $lineContents[0];
     my $position     = $lineContents[1];
     my $a1           = $lineContents[3];
     my $a2           = $lineContents[4];
     my $key          = $chr."_".$position."_".$a1."_".$a2;
-
-    if(length($a1) > 1 || length($a2) > 1)
-    {
-        next;
-    }
 
     if(exists $dict->{$key})
     {
