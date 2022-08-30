b91ebbd81ee668fc8723cbe90ebd8629c44a4629
vaqm2 <appadurai.vivek@gmail.com>
2022-08-30 11:05:27 +0200

    new code

--- txt_to_npz.py
+++ txt_to_npz.py
@@ -3,9 +3,9 @@
 import sys
 import numpy as np
 
-print("Reading LD file" + sys.argv[1] + "..")
+print("Reading LD file " + sys.argv[1] + " ..")
 with open(sys.argv[1], "r") as ld_file:
     ld_matrix = ld_file.readlines()
 
-print("Writing to .npz" + sys.argv[2] + "..")
+print("Writing to .npz " + sys.argv[2] + " ..")
 np.savez(sys.argv[2], ld_matrix)
