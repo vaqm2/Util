6edcd56af8c10705c0b82345cb20e765f046a675
vaqm2 <appadurai.vivek@gmail.com>
2022-10-14 14:27:01 +0200

    restructure code to output coordinates first

--- get_clump_regions.pl
+++ get_clump_regions.pl
@@ -8,8 +8,10 @@ use List::Util qw(min max);
 
 my ($clump, $bim);
 
-GetOptions("clump=s" => \$clump,
-    "bim=s" => \$bim);
+GetOptions(
+    "clump=s" => \$clump,
+    "bim=s"   => \$bim
+);
 
 my $usage = "\nUSAGE: perl $0 --clump plink.clumped --bim reference.bim\n\n";
 
@@ -24,8 +26,8 @@ my $fh = IO::File->new($bim) || die "FATAL: Cannot open BIM file: $bim"."!!\n";
 
 while(my $line = $fh->getline) {
     chomp($line);
-    my @lineContents = split(/\s+/, $line);
-    my $snp = $lineContents[1];
+    my @lineContents         = split(/\s+/, $line);
+    my $snp                  = $lineContents[1];
     $snp_coordinates->{$snp} = $lineContents[3];
 }
 
@@ -33,13 +35,14 @@ $fh->close;
 
 my $nh = IO::File->new($clump) || die "FATAL: Cannot open clumped file: $clump"."!!\n";
 
-print "INDEX_SNP"." ";
 print "CHR"." ";
-print "BP"." ";
-print "P"." ";
-print "N_TAGS"." ";
+print "BUFFER_START"." ";
+print "BUFFER_END"." ";
 print "START"." ";
-print "END"."\n";
+print "END";
+print "INDEX_SNP"." ";
+print "BP"." ";
+print "P"."\n";
 
 while(my $line = $nh->getline) {
     chomp($line);
@@ -48,37 +51,44 @@ while(my $line = $nh->getline) {
     }
     else {
         my @lineContents = split(/\s+/, $line);
-        print $lineContents[2]." ";
-        print $lineContents[0]." ";
-        print $lineContents[3]." ";
-        print $lineContents[4]." ";
-        print $lineContents[6]." ";
+        my $chromosome   = $lineContents[0];
+        my $index_snp    = $lineContents[2];
+        my $bp           = $lineContents[3];
+        my $start        = $bp; # Defaults
+        my $end          = $bp; # Defaults
+        my $buffer_start = $bp - 50000; # Defaults
+        my $buffer_end   = $bp + 50000; #Defaults
+        my $p            = $lineContents[4];
 
-        if($lineContents[11] eq "NONE") {
-            print $lineContents[3]." ";
-            print $lineContents[3];
-        }
-        else {
+        if($lineContents[11] ne "NONE") {
             my @tags = split(/\,/, $lineContents[11]);
             my @tag_coordinates;
 
             for my $index(0..$#tags) {
-                $tags[$index] =~ s/\(.*$//;
-                if(exists $snp_coordinates->{$tags[$index]}) {
-                    $tag_coordinates[$index] = $snp_coordinates->{$tags[$index]};
+                my $tag_snp = $tags[$index];
+                $tag_snp    =~ s/\(.*$//;
+                if(exists $snp_coordinates->{$tag_snp}) {
+                    $tag_coordinates[$index] = $snp_coordinates->{$tag_snp};
                 }
                 else {
                     die "FATAL: Cannot find coordinate for tag SNP: $tags[$index]"."!!\n";
                 }
             }
 
-            my $start = min(@tag_coordinates);
-            my $end = max(@tag_coordinates);
-
-            print $start." ";
-            print $end;
+            $start        = min(@tag_coordinates);
+            $end          = max(@tag_coordinates);
+            $buffer_start = $start - 50000;
+            $buffer_end   = $end + 50000;
         }
-        print "\n";
+
+        print $chromosome." ";
+        print $buffer_start." ";
+        print $buffer_end." ";
+        print $start." ";
+        print $end." ";
+        print $index_snp." ";
+        print $bp." ";
+        print $p."\n";
     }
 }
 
