2c4859dd59db567e1c6653bcae7998248ac54315
vaqm2 <appadurai.vivek@gmail.com>
2022-10-19 16:36:57 +0200

    new code

--- finemap_indices.R
+++ finemap_indices.R
@@ -9,6 +9,7 @@ regions           = fread(args[2], header = F)
 colnames(regions) = c("CHROMOSOME", "START", "END", "SNP", "BP", "P")
 regions           = regions %>% select("SNP", "START", "END")
 finemap_assoc     = left_join(assoc, regions, by = c("SNP")) %>%
-    select(CHROMOSOME, BP, START, END, A1, A2, MAF, BETA, SE, P, Q)
+    select(CHROMOSOME, BP, START, END, A1, A2, MAF, BETA, SE, P, Q) %>%
+    arrange(Q)
 
 write.table(args[3], row.names = F, quote = F, sep = " ")
