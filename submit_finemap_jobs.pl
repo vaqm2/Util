5474b9afca16d8b8b7a4f9d56a85646ce17aeae2
vaqm2 <appadurai.vivek@gmail.com>
2022-11-10 09:55:18 +0100

    finemaps from genotyps, not ldm

--- submit_finemap_jobs.pl
+++ submit_finemap_jobs.pl
@@ -4,25 +4,27 @@ use strict;
 use warnings;
 use IO::File;
 
-my $sumstats      = $ARGV[0];
-my $ld_matrix     = $ARGV[1];
-my $n             = $ARGV[2];
-my $ld_mat_prefix = $ld_matrix;
-$ld_mat_prefix    =~ s/\.bcor$//;
-my @coords        = split(/\_/, $ld_mat_prefix);
-my $chr           = $coords[$#coords - 2];
-my $start         = $coords[$#coords - 1];
-my $end           = $coords[$#coords];
+my $sumstats = $ARGV[0];
+my $regions  = $ARGV[1];
+my $n        = $ARGV[2];
+my $prefix   = $sumstats;
+$prefix      =~ s/\.parquet//;
 
-if($start < 0) {
-    $start = 0;
-}
+my $fh = IO::File->new($ARGV[1]) || die "ERROR: Cannot open regions file: $ARGV[1]\n!!";
 
-my $sys_command = "sbatch --error=$ld_mat_prefix.err --output=$ld_mat_prefix.out --job-name=$ld_mat_prefix";
-$sys_command   .= " --mem=6g --time=48:00:00 --account=cross_disorder_2";
-$sys_command   .= " --wrap=\"python /faststorage/jail/project/cross_disorder_2/scripts/polyfun/finemapper.py";
-$sys_command   .= " --sumstats $sumstats --chr $chr --n $n --start $start --end $end --method susie";
-$sys_command   .= " --max-num-causal 5 --out ${ld_mat_prefix}.PIPs.txt --non-funct --memory 32";
-$sys_command   .= " --cache-dir Finemapper_LD_Cache --verbose --allow-missing --ld BCOR/$ld_matrix\"";
+while(my $line = $fh->getline) {
+    chomp($line);
+    my ($chr, $start, $end) = split(/\s+/, $line);
+    $prefix .= $chr."_".$start."_".$end;
+
+    my $sys_command = "sbatch --error=${prefix}.err --output=${prefix}.out --job-name=$prefix";
+    $sys_command   .= " --mem=32g --time=12:00:00 --account=cross_disorder_2";
+    $sys_command   .= " --wrap=\"python /faststorage/jail/project/cross_disorder_2/scripts/polyfun/finemapper.py";
+    $sys_command   .= " --sumstats $sumstats --chr $chr --n $n --start $start --end $end --method susie";
+    $sys_command   .= " --max-num-causal 5 --out ${prefix}.PIPs.txt --non-funct --memory 32";
+    $sys_command   .= " --cache-dir Finemapper_LD_Cache --verbose --allow-missing";
+    $sys_command   .= " --geno /faststorage/jail/project/ibp_data_ipsych/ipsych_2012/iPSYCH_IBP_Imputed_v_2.1/qced/plink1/";
+    $sys_command   .= "iPSYCH2012.PhaseBEAGLE5.1PhaseStates560ImputeBEAGLE5.1.chr${chr}.SNP_SAMPLE_QC.UpdatedRSID.1\"";
 
 `$sys_command`;
+}
