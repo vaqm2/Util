0bb41ac6505dbe4ebb2b61b87f0d6d6aa30d9414
vaqm2 <appadurai.vivek@gmail.com>
2022-11-15 08:38:43 +0100

    new code

---
+++ select_high_qual_snps.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use IO::File;
+
+my $high_quality_snps = {};
+
+open(IN, "zcat $ARGV[0] |");
+
+while(<IN>) {
+    chomp($_);
+    my @lineContents = split(/\s+/, $_);
+    my $chrom        = $lineContents[0];
+    my $position     = $lineContents[1];
+    my $snp_id       = $lineContents[2];
+    my $ref          = $lineContents[3];
+    my $alt          = $lineContents[4];
+    my $maf          = $lineContents[5];
+    my $dr2          = $lineContents[7];
+
+    if($maf > 0.5) {
+        $maf = 1 - $maf;
+    }
+
+    if($maf >= 0.01 && $dr2 >= 0.8) {
+        $high_quality_snps->{$snp_id} = 1; 
+    }
+}
+
+close(IN);
+
+open(IN, "zcat $ARGV[1] |");
+
+while(<IN>) {
+    chomp($_);
+    my @lineContents = split(/\s+/, $_);
+    my $chrom        = $lineContents[0];
+    my $position     = $lineContents[1];
+    my $snp_id       = $lineContents[2];
+    my $ref          = $lineContents[3];
+    my $alt          = $lineContents[4];
+    my $maf          = $lineContents[5];
+    my $dr2          = $lineContents[7];
+
+    if(!exists $high_quality_snps->{$snp_id}) {
+        next;
+    }
+
+    if($maf > 0.5) {
+        $maf = 1 - $maf;
+    }
+
+    if($maf > 0.01 && $dr2 >= 0.8) {
+        $high_quality_snps->{$snp_id} += 1;
+    }
+}
+
+close(IN);
+
+my $fh = IO::File->new($ARGV[2]);
+
+while(my $line = $fh->getline) {
+    chomp($line);
+    my @lineContents = split(/\s+/, $line);
+    my $snp_id = $lineContents[2];
+
+    if(!exists $high_quality_snps->{$snp_id}) {
+        next;
+    }
+
+    if($high_quality_snps->{$snp_id} == 1) {
+        next;
+    }
+
+    if($high_quality_snps->{$snp_id} > 2) {
+        print STDERR "WARN: Duplicate SNP: $snp_id"."\n";
+    }
+
+    if($high_quality_snps->{$snp_id} == 2) {
+        print $line."\n";
+    }
+}
+
+$fh->close;
