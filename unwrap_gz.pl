013c4b7975c867991360f3ef85717177eff7e4d0
vaqm2 <appadurai.vivek@gmail.com>
2023-01-09 11:30:34 +0100

    new code

---
+++ unwrap_gz.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use IO::File;
+
+my $outFile = $ARGV[0];
+$outFile =~ s/^.*UpdatedRSID\.//;
+$outFile =~ s/\.gz$//;
+$outFile =~ s/\./\_/g;
+$outFile = "iPSYCH2015.".$outFile.".z";
+
+my $out = IO::File->new("> $outFile") || die "FATAL: Cannot write to output file: $outFile!\n";
+open(IN, "zcat $ARGV[0] |");
+
+while(<IN>) {
+    print $out $_;
+}
+
+close(IN);
