1f043712872bd0780a32d1dae99b214b38f7ccae
vaqm2 <appadurai.vivek@gmail.com>
2023-01-09 09:58:17 +0100

    added functionality

--- rename_ext.pl
+++ rename_ext.pl
@@ -9,11 +9,15 @@ my $fh = IO::File->new($ARGV[0]) || die "Cannot open file: $ARGV[0]!\n";
 while(my $line = $fh->getline) {
     chomp($line);
     my $alt_name = $line;
+    $line =~ s/\..*$//;
     $alt_name =~ s/\..*$//;
     $alt_name =~ s/^ \D+2015\D+ //x;
-    $alt_name = "iPSYCH2015_".$alt_name.".z";
+    $alt_name = "iPSYCH2015_".$alt_name;
 
-    print $line." ".$alt_name."\n";
+    `mv $line.z $alt_name.z`;
+    `mv $line.bgen $alt_name.bgen`;
+    `mv $file.rsids.txt $alt_name.rsids.txt`;
+    `mv $file.bgen.bgi $alt_name.bgen.bgi`;
 }
 
 $fh->close;
