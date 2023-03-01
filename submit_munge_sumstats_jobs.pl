ed23a718c444d2ae8c954bfd1cf459bf11795157
vaqm2 <appadurai.vivek@gmail.com>
2023-03-01 13:44:51 +0100

    added chunk size

--- submit_munge_sumstats_jobs.pl
+++ submit_munge_sumstats_jobs.pl
@@ -31,7 +31,7 @@ while(my $line = $fh->getline) {
             $cmd .= " --a1-inc";
             $cmd .= " --merge-alleles /faststorage/project/xdx2/data/w_hm3.snplist";
             $cmd .= " --out iPSYCH2015_EUR_${trait_name}";
-            $cmd .= " --chunk-size 50000";
+            $cmd .= " --chunksize 50000";
 
             `sbatch --error=munge_${trait_name}.err --output=munge_${trait_name}.out --job-name=munge_${trait_name} --mem=8g --time=04:00:00 --account=xdx2 --wrap=\"$cmd\";`
         }
