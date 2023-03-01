8c7e94209f509c1e7b03343e4792f9e31ac97211
vaqm2 <appadurai.vivek@gmail.com>
2023-03-01 12:36:12 +0100

    new code

---
+++ submit_munge_sumstats_jobs.pl
@@ -0,0 +1,41 @@
+#/usr/bin/env perl
+
+use strict;
+use warnings;
+use IO::File;
+
+my $fh = IO::File->new("GWAS_N.csv") || die "ERROR: Cannot open file: GWAS_N.csv!\n";
+
+while(my $line = $fh->getline) {
+    chomp($line);
+    if($line =~ /^PHENO/) {
+        next;
+    }
+    else {
+        my @lineContents = split(/\,/, $line);
+        my $trait_name = $lineContents[0];
+        my $cohort = $lineContents[1];
+        my $n_cases = $lineContents[2];
+        my $n_samples = $lineContents[3];
+
+        if($cohort == "2015") {
+            my $cmd = "python /faststorage/jail/project/xdx2/scripts/ldsc/munge_sumstats.py";
+            $cmd .= " --sumstats /faststorage/jail/project/xdx2/data/assoc/iPSYCH2015_EUR_${trait_name}.assoc";
+            $cmd .= " --N $n_samples";
+            $cmd .= " --N-cas $n_cases";
+            $cmd .= " --a1 A1";
+            $cmd .= " --a2 A2";
+            $cmd .= " --p P";
+            $cmd .= " --frq MAF";
+            $cmd .= "--signed-sumstats Z,0";
+            $cmd .= " --a1-inc";
+            $cmd .= " --merge-alleles /faststorage/jail/project/xdx2/data/w_hm3.snplist";
+            $cmd .= " --out iPSYCH2015_EUR_${trait_name}";
+
+            `sbatch --error=munge_${trait_name}.err --output=munge_${trait_name}.out --jobname=munge_${trait_name} \
+            --mem=8g --time=04:00:00 --account=xdx2 --wrap=\"$cmd\";`
+        }
+    }
+}
+
+$fh->close;
