847b302d1a5f06c8c8e15b6b6adfcab462467ae8
vaqm2 <appadurai.vivek@gmail.com>
2023-04-24 11:57:44 +0200

    subset before testing

--- magma_protein_coding_only.R
+++ magma_protein_coding_only.R
@@ -6,10 +6,18 @@ library(dplyr)
 
 load("/faststorage/project/xdx2/data/magma/geneAnno_allgenes.rda")
 geneAnno1 = geneAnno1 %>% 
-    filter(gene_biotype == "protein_coding") %>% 
+    filter(gene_biotype == "protein_coding" & hgnc_symbol != "") %>% 
     select(ensembl_gene_id, hgnc_symbol)
-colnames(geneAnno1) = c("ENSEMBL_ID", "GENE")
-result = read.table(args[1], header = T)
-result = result %>% rename(ENSEMBL_ID = GENE)
-result = inner_join(result, geneAnno1, by = c("ENSEMBL_ID"))
-write.table(result, args[2], row.names = F, quote = F, sep = " ")
+
+genes_to_test = read.table(args[1], header = F, fill = T)
+genes_to_test = genes_to_test %>% rename(ensembl_gene_id = V1)
+
+genes_to_test = inner_join(geneAnno1, genes_to_test) %>% 
+    select(-ensembl_gene_id)
+
+write.table(genes_to_test, 
+            args[2], 
+            row.names = F, 
+            quote = F, 
+            sep = " ", 
+            header = F)
