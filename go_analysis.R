ac6d3c1ffd7b2bce2b8306cd2054a6a610b9905e
vaqm2 <appadurai.vivek@gmail.com>
2023-04-26 15:21:11 +0200

    updates and improvements

--- go_analysis.R
+++ go_analysis.R
@@ -6,23 +6,18 @@ library(gprofiler2)
 args = commandArgs(trailingOnly = TRUE)
 
 genes = read.table(args[1], header = TRUE)
-genes_selected = genes %>% 
-    mutate(P_ADJ = p.adjust(P, method = c("fdr"))) %>%
-    filter(P_ADJ <= 0.1) %>% 
-    arrange(desc(abs(ZSTAT)))
-gost_out = gost(query = genes_selected$GENE, 
+gost_out = gost(query = genes$GENE, 
                 organism = "hsapiens", 
                 ordered_query = TRUE, 
                 significant = TRUE,
                 user_threshold = 0.05, 
                 correction_method = "fdr", 
-                sources = c("GO", "KEGG", "REAC"))
+                sources = c("GO"))
 gost_result = gost_out$result %>% 
     filter(term_size >= 15 & term_size <= 600 & intersection_size >= 5) %>%
     arrange(p_value) %>%
-    as.data.frame()
-gost_result$parents = paste(gost_result$parents, collapse = ",")
-gost_result$evidence_codes = paste(gost_result$evidence_codes, collapse = ",")
+    as.data.frame() %>% 
+    select(-parents, -evidence_codes)
 
 write.table(gost_result,
             paste0(args[2], "_Enrichments.txt"), 
