370767649db9c580e5cd6a3e539b1bc1de88f99e
vaqm2 <appadurai.vivek@gmail.com>
2023-04-27 10:30:49 +0200

    test significant genes against background

--- go_analysis.R
+++ go_analysis.R
@@ -6,19 +6,24 @@ library(gprofiler2)
 args = commandArgs(trailingOnly = TRUE)
 
 genes = read.table(args[1], header = TRUE)
-gost_out = gost(query = genes$GENE, 
+genes_associated = genes %>% 
+    mutate(P_ADJ = p.adjust(P, method = c("fdr"))) %>%
+    filter(P_ADJ < 0.1) %>%
+    select(GENE) %>%
+    arrange(P_ADJ)
+genes_background = genes %>% select(GENE) %>% sample()
+gost_out = gost(query = genes_associated$GENE, 
                 organism = "hsapiens", 
                 ordered_query = TRUE, 
                 significant = TRUE,
                 user_threshold = 0.05, 
-                correction_method = "fdr", 
+                correction_method = "fdr",
+                custom_bg = genes_background$GENE,
                 evcodes = FALSE,
                 sources = c("GO"))
 gost_result = gost_out$result %>% 
-    filter(term_size >= 15 & term_size <= 600 & intersection_size >= 5) %>%
     arrange(p_value) %>%
-    as.data.frame() %>% 
-    select(-parents)
+    as.data.frame()
 
 write.table(gost_result,
             paste0(args[2], "_Enrichments.txt"), 
