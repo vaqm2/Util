92d3a6f2dfa7fa27b4d9cc82eb5e2578a25ebb9e
vaqm2 <appadurai.vivek@gmail.com>
2023-04-27 11:01:13 +0200

    testing logging

--- go_analysis.R
+++ go_analysis.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(gprofiler2)
 library(logr)
 
-log_file = paste0(args[2], ".log")
+log_file = paste0(as.character(args[2]), ".log")
 log_open(log_file)
 
 args = commandArgs(trailingOnly = TRUE)
