7555539365ab4fab920a769c8ee453bb597252bf
vaqm2 <appadurai.vivek@gmail.com>
2023-04-27 16:14:37 +0200

    go terms for top 500 genes

--- go_analysis.R
+++ go_analysis.R
@@ -14,23 +14,21 @@ write(paste("SOURCE", "TERM", "TERM_SIZE", "INTERSECTION_SIZE", "P_FDR", "TEST",
       append = TRUE)
 
 for (file in files) {
+    test = gsub("^iPSYCH2015_EUR_", "", file)
+    test = gsub("\\.*$", "", test)
     print(paste0("Processing", " ", file, "..."))
     genes = read.table(file, header = TRUE)
     genes_selected = genes %>%
         arrange(desc(abs(ZSTAT))) %>%
-        head(500) %>%
         select(GENE)
-    genes_background = genes %>% 
-        select(GENE) %>% 
-        sample()
     gost_out = gost(query = genes_selected$GENE, 
                     organism = "hsapiens", 
                     ordered_query = TRUE, 
                     significant = TRUE,
                     user_threshold = 0.05, 
                     correction_method = "fdr",
-                    custom_bg = genes_background$GENE,
                     evcodes = FALSE,
+                    domain_scope = "annotated",
                     sources = c("GO"))
     if(!is.null(gost_out$result)) {
         gost_result = gost_out$result %>% 
@@ -38,7 +36,7 @@ for (file in files) {
             as.data.frame() %>%
             filter(term_size >= 15 & term_size <= 600 & intersection_size >= 5) %>%
             select(source, term_name, term_size, intersection_size, p_value) %>% 
-            mutate(query = args[1])
+            mutate(query = test)
     
         write.table(gost_result,
                     out_file, 
