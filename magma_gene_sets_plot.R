74318d5ec10e8c235251a0605173fb8479e82cf9
vaqm2 <appadurai.vivek@gmail.com>
2023-05-03 16:05:35 +0200

    debug

--- magma_gene_sets_plot.R
+++ magma_gene_sets_plot.R
@@ -61,7 +61,7 @@ png(paste0(args[1], "_xDx.png"), res = 300, width = 8, height = 8, units = "in")
 
 ggplot(xdx, aes(y = FULL_NAME, x = -log10(P))) +
     geom_bar(stat = "identity") +
-    scale_y_discrete(labels = label_wrap(10)) +
+    scale_y_discrete(labels = label_wrap(20)) +
     theme_classic() +
     xlab("") +
     ylab("")
@@ -71,8 +71,8 @@ dev.off()
 png(paste0(args[1], "_Case_Case.png"), res = 300, width = 12, height = 12, units = "in")
 
 ggplot(case_case, aes(y = FULL_NAME, x = -log10(P), fill = GWAS)) + 
-    geom_bar(stat = "identity") +
-    scale_y_discrete(labels = label_wrap(10)) + 
+    geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
+    scale_y_discrete(labels = label_wrap(20)) + 
     theme_classic() + 
     facet_wrap(TRAIT ~ ., scales = "free", ncol = 2) + 
     scale_fill_manual(values = c("red", "blue")) +
@@ -83,9 +83,9 @@ dev.off()
 
 png(paste0(args[1], "_Pairwise.png"), res = 300, width = 15, height = 15, units = "in")
 
-ggplot(pairwise, aes(y = FULL_NAME, x = -log10(P), fill = GWAS)) + 
+ggplot(pairwise, aes(y = FULL_NAME, x = -log10(P))) + 
     geom_bar(stat = "identity") +
-    scale_y_discrete(labels = label_wrap(10)) +
+    scale_y_discrete(labels = label_wrap(20)) +
     theme_classic() + 
     xlab("") + 
     ylab("") + 
