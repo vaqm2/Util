a0f17134881c3eb6f36781dd7ea988f98157818f
vaqm2 <appadurai.vivek@gmail.com>
2023-05-15 13:11:53 +0200

    go_anlaysis update

--- go_analysis.R
+++ go_analysis.R
@@ -8,7 +8,21 @@ files = list.files(path = getwd(), pattern = "*.genes.out")
 out_file = paste0(args[1], "_Enrichments.txt")
 file.create(out_file)
 
-write(paste("SOURCE", "TERM", "TERM_SIZE", "INTERSECTION_SIZE", "P_FDR", "TEST", 
+write(paste("query", 
+            "significant", 
+            "p_value",
+            "term_size", 
+            "query_size", 
+            "intersection_size", 
+            "precision",
+            "recall",
+            "term_id",
+            "source",
+            "term_name",
+            "effective_domain_size",
+            "source_order",
+            "parents",
+            "evidence_codes",
             sep = "\t"), 
       out_file,
       append = TRUE)
@@ -19,24 +33,27 @@ for (file in files) {
     print(paste0("Processing", " ", file, "..."))
     genes = read.table(file, header = TRUE)
     genes_selected = genes %>%
+        mutate(P_ADJ = p.adjust(P, method = c("fdr"))) %>%
+        filter(P_ADJ <= 0.05) %>% 
         arrange(desc(abs(ZSTAT))) %>%
         select(GENE)
     gost_out = gost(query = genes_selected$GENE, 
                     organism = "hsapiens", 
                     ordered_query = TRUE, 
-                    significant = FALSE,
+                    significant = TRUE,
                     user_threshold = 0.05, 
-                    correction_method = "bonferroni",
-                    evcodes = FALSE,
+                    correction_method = "g_SCS",
+                    evcodes = TRUE,
                     domain_scope = "annotated",
-                    sources = c("GO"))
+                    custon_bg = genes$GENE,
+                    sources = c("GO", "KEGG"))
     if(!is.null(gost_out$result)) {
         gost_result = gost_out$result %>% 
             arrange(p_value) %>%
             as.data.frame() %>%
-            filter(term_size >= 15 & term_size <= 600 & intersection_size >= 5) %>%
-            select(source, term_name, term_size, intersection_size, p_value) %>% 
-            mutate(query = test)
+            filter(term_size >= 15 & term_size <= 600 & intersection_size >= 5)
+        
+        gost_result$query = test
     
         write.table(gost_result,
                     out_file, 
