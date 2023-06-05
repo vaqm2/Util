b8fa45bc67f5846dcf940e58418582d25f50aa34
vaqm2 <appadurai.vivek@gmail.com>
2023-06-05 11:45:17 +0200

    debug

--- gmt_to_txt.py
+++ gmt_to_txt.py
@@ -8,5 +8,5 @@ for set in gmt:
     set_contents = set.rstrip().split()
     set_name = set_contents[0]
     for gene in set_contents[2:]:
-        print(f'{set_name}\t{gene}\n')
+        print(f'{set_name}\t{gene}')
 gmt.close()
