7d6440ef015a1efb9c81c8404b7b6692f7cb3222
vaqm2 <appadurai.vivek@gmail.com>
2023-06-05 11:53:27 +0200

    debug

--- gmt_to_txt.py
+++ gmt_to_txt.py
@@ -6,7 +6,8 @@ gmt = open(sys.argv[1], 'r')
 
 for set in gmt:
     set_contents = set.rstrip().split()
-    set_name = set_contents[0]
-    for gene in set_contents[2:]:
-        print(f'{set_name}\t{gene}')
+    if 10 <= len(set_contents) <= 500:
+        set_name = set_contents[0]
+        for gene in set_contents[2:]:
+            print(f'{set_name}\t{gene}')
 gmt.close()
