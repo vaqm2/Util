0c8dee0aa06956609d34d03bbd2a917353279b69
vaqm2 <appadurai.vivek@gmail.com>
2023-06-05 11:41:32 +0200

    debug

--- gmt_to_txt.py
+++ gmt_to_txt.py
@@ -2,10 +2,9 @@
 
 import sys
 
-gmt = open(sys.argv[1], "r")
+gmt = open(sys.argv[1], 'r')
 
-for set in gmt.readline():
-    print(set)
+for set in gmt:
     set_contents = set.split('\s+')
     set_name = set_contents[0]
     for gene in set_contents[2:]:
