1aff558d69eb32a6a23f02d928e5532015ef7d60
vaqm2 <appadurai.vivek@gmail.com>
2023-06-05 11:17:52 +0200

    code converts gmt sets to text for magma

---
+++ gmt_to_txt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+gmt = open(sys.argv[1], "r")
+
+for set in gmt.readline:
+    set_contents = set.split("\s+")
+    set_name = set_contents[0]
+    for gene in set_contents[2:]:
+        print(set_name + "\t" + gene + "\n")
+gmt.close()
