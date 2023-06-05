679e7130ab0ecc89dcecd4acd9ef400bb2df1f17
vaqm2 <appadurai.vivek@gmail.com>
2023-06-05 14:00:06 +0200

    code to restrict gene set sizes

---
+++ restrict_set_size.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+
+sets_df = pd.read_csv(sys.argv[1], sep = '\s+', header = None, names = ["SET", "GENE"])
+gene_sets_10_500 = sets_df.groupby("SET").filter(lambda x: 10 <= len(x) <= 500)
+gene_sets_10_500.to_csv(sys.argv[2], sep = '\t', header = None)
